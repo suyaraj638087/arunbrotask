3e7e142de8d6a641b5454c34b2aee0fcef28b524
suyaraj638087 <suyarajjeyapaul@gmail.com.com>
2023-04-17 00:36:01 +0530

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+$('.whole').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
